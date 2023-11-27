877587e40dfef7db0d5c901d2704d6972d3e973f
Mohammed Izzath <117861380+mizzath2003@users.noreply.github.com>
2023-11-27 13:00:58 +0530

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -20,7 +20,7 @@ export const shazamCoreApi = createApi({
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
-        "37004310c7mshc6d9eb6fe5a41a0p1c0a77jsn554b78aa9864"
+        /* "Your_API_key_here" */
       );
 
       return headers;
