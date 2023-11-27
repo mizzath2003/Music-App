10ef04e304a91f85d0b388ac4d6a1984e0f874e7
Mohammed Izzath <117861380+mizzath2003@users.noreply.github.com>
2023-11-27 13:01:35 +0530

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,17 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-/* const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "8eb3e4c348msh6a6cd4a79e4085ap1eec94jsn046a7082c51a",
-    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
-  },
-};
-
-fetch("https://shazam.p.rapidapi.com/charts/track", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err)); */
 
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
